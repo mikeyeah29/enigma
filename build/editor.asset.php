65c0f9a2bd3a434773e59eaecb9c1b29e9134f92
Mike Rockett <mikerockett@live.com>
2026-02-18 16:48:37 +0000

    cd

--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'a5b378f4d76984247d98');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'aa33a8b6dc47f109da42');
